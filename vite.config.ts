95a6cfe5819efa382e35b093d25d23a88e5e3f1f
pengYYY <pengyue970715@gmail.com>
2021-12-24 11:20:51 +0800

    fix(base url): remove base url

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/starter/vue-next/',
+  base: './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
