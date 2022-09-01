60b45548c9c4123dfdf321dd16932156594239d7
PDieE <23275429@qq.com>
2022-09-01 15:29:09 +0800

    feat: 使用持久化插件将store持久化
    #278

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,8 @@
 import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 
 const store = createPinia();
+store.use(piniaPluginPersistedstate);
 
 export { store };
 
