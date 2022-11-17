586de9af30fba7fe2a37ce0743fd856b6e41d2ff
tnt group <dodu@live.cn>
2022-11-17 16:56:42 +0800

    fix: 修复显示标签页时候布局错乱的问题

--- src/layouts/index.vue
+++ src/layouts/index.vue
@@ -4,7 +4,7 @@
       <t-layout key="side" :class="mainLayoutCls">
         <t-aside><layout-side-nav /></t-aside>
         <t-layout>
-          <t-header><layout-header /></t-header>
+          <t-header height="64px"><layout-header /></t-header>
           <t-content><layout-content /></t-content>
         </t-layout>
       </t-layout>
@@ -12,7 +12,7 @@
 
     <template v-else>
       <t-layout key="no-side">
-        <t-header><layout-header /> </t-header>
+        <t-header height="64px"><layout-header /> </t-header>
         <t-layout :class="mainLayoutCls">
           <layout-side-nav />
           <layout-content />
