5b9a418600c00624d29d9bcbf0d5b309b846d6d3
yuyang <uyarnchen@gmail.com>
2023-01-16 18:54:21 +0800

    feat: add backend dynamic router permission code (#394)
    * chore: 大致雏形
    
    * feat: complete permission code
    
    * feat: 修复渲染问题并完善权限代码
    
    * chore: release 0.7.0
    
    * chore: 更新依赖
    
    * chore: fix echarts version
    
    * feat: upgrade dependencies
    
    * chore: mock route map
    
    * chore: hash router
    
    * chore: concat fixed routers
    
    * chore: fix dependencies
    
    * feat: optimize
    
    * chore: remove roles from get menu process
    
    * chore: remain fe permssion code
    
    Co-authored-by: 悠静萝莉 <i@mikuhl.cn>

--- mock/index.ts
+++ mock/index.ts
@@ -144,4 +144,174 @@ export default [
       },
     },
   },
+  {
+    url: '/api/get-menu-list',
+    method: 'get',
+    timeout: 2000,
+    response: {
+      code: 0,
+      data: {
+        ...Mock.mock({
+          list: [
+            {
+              path: '/list',
+              name: 'list',
+              component: 'LAYOUT',
+              redirect: '/list/base',
+              meta: {
+                title: '列表页',
+                icon: 'view-list',
+              },
+              children: [
+                {
+                  path: 'base',
+                  name: 'ListBase',
+                  component: '/list/base/index',
+                  meta: {
+                    title: '基础列表页',
+                  },
+                },
+                {
+                  path: 'card',
+                  name: 'ListCard',
+                  component: '/list/card/index',
+                  meta: {
+                    title: '卡片列表页',
+                  },
+                },
+                {
+                  path: 'filter',
+                  name: 'ListFilter',
+                  component: '/list/filter/index',
+                  meta: {
+                    title: '筛选列表页',
+                  },
+                },
+                {
+                  path: 'tree',
+                  name: 'ListTree',
+                  component: '/list/tree/index',
+                  meta: {
+                    title: '树状筛选列表页',
+                  },
+                },
+              ],
+            },
+            {
+              path: '/form',
+              name: 'form',
+              component: 'LAYOUT',
+              redirect: '/form/base',
+              meta: {
+                title: '表单页',
+                icon: 'edit-1',
+              },
+              children: [
+                {
+                  path: 'base',
+                  name: 'FormBase',
+                  component: '/form/base/index',
+                  meta: {
+                    title: '基础表单页',
+                  },
+                },
+                {
+                  path: 'step',
+                  name: 'FormStep',
+                  component: '/form/step/index',
+                  meta: {
+                    title: '分步表单页',
+                  },
+                },
+              ],
+            },
+            {
+              path: '/detail',
+              name: 'detail',
+              component: 'LAYOUT',
+              redirect: '/detail/base',
+              meta: {
+                title: '详情页',
+                icon: 'layers',
+              },
+              children: [
+                {
+                  path: 'base',
+                  name: 'DetailBase',
+                  component: '/detail/base/index',
+                  meta: {
+                    title: '基础详情页',
+                  },
+                },
+                {
+                  path: 'advanced',
+                  name: 'DetailAdvanced',
+                  component: '/detail/advanced/index',
+                  meta: {
+                    title: '多卡片详情页',
+                  },
+                },
+                {
+                  path: 'deploy',
+                  name: 'DetailDeploy',
+                  component: '/detail/deploy/index',
+                  meta: {
+                    title: '数据详情页',
+                  },
+                },
+                {
+                  path: 'secondary',
+                  name: 'DetailSecondary',
+                  component: '/detail/secondary/index',
+                  meta: {
+                    title: '二级详情页',
+                  },
+                },
+              ],
+            },
+            {
+              path: '/frame',
+              name: 'Frame',
+              component: 'Layout',
+              redirect: '/frame/doc',
+              meta: {
+                icon: 'internet',
+                title: '外部页面',
+              },
+              children: [
+                {
+                  path: 'doc',
+                  name: 'Doc',
+                  component: 'IFrame',
+                  meta: {
+                    frameSrc: 'https://tdesign.tencent.com/starter/docs/vue-next/get-started',
+                    title: '使用文档（内嵌）',
+                  },
+                },
+                {
+                  path: 'TDesign',
+                  name: 'TDesign',
+                  component: 'IFrame',
+                  meta: {
+                    frameSrc: 'https://tdesign.tencent.com/vue-next/getting-started',
+                    title: 'TDesign 文档（内嵌）',
+                  },
+                },
+                {
+                  path: 'TDesign2',
+                  name: 'TDesign2',
+                  component: 'IFrame',
+                  meta: {
+                    frameSrc: 'https://tdesign.tencent.com/vue-next/getting-started',
+                    frameBlank: true,
+                    title: 'TDesign 文档（外链）',
+                  },
+                },
+              ],
+            },
+          ],
+        }),
+      },
+    },
+  },
 ] as MockMethod[];
--- src/store/modules/permission.ts
+++ src/store/modules/permission.ts
@@ -1,59 +1,41 @@
 import { defineStore } from 'pinia';
 import { RouteRecordRaw } from 'vue-router';
-import router, { asyncRouterList } from '@/router';
+import router, { fixedRouterList, homepageRouterList } from '@/router';
 import { store } from '@/store';
-
-function filterPermissionsRouters(routes: Array<RouteRecordRaw>, roles: Array<unknown>) {
-  const res = [];
-  const removeRoutes = [];
-  routes.forEach((route) => {
-    const children = [];
-    route.children?.forEach((childRouter) => {
-      const roleCode = childRouter.meta?.roleCode || childRouter.name;
-      if (roles.indexOf(roleCode) !== -1) {
-        children.push(childRouter);
-      } else {
-        removeRoutes.push(childRouter);
-      }
-    });
-    if (children.length > 0) {
-      route.children = children;
-      res.push(route);
-    }
-  });
-  return { accessedRouters: res, removeRoutes };
-}
+import { RouteItem } from '@/api/model/permissionModel';
+import { getMenuList } from '@/api/permission';
+import { transformObjectToRoute } from '@/utils/route';
 
 export const usePermissionStore = defineStore('permission', {
   state: () => ({
     whiteListRouters: ['/login'],
     routers: [],
     removeRoutes: [],
+    asyncRoutes: [],
   }),
   actions: {
-    async initRoutes(roles: Array<unknown>) {
-      let accessedRouters = [];
+    async initRoutes() {
+      const accessedRouters = this.asyncRoutes;
 
-      let removeRoutes = [];
-      // special token
-      if (roles.includes('all')) {
-        accessedRouters = asyncRouterList;
-      } else {
-        const res = filterPermissionsRouters(asyncRouterList, roles);
-        accessedRouters = res.accessedRouters;
-        removeRoutes = res.removeRoutes;
-      }
-
-      this.routers = accessedRouters;
-      this.removeRoutes = removeRoutes;
-
-      removeRoutes.forEach((item: RouteRecordRaw) => {
-        if (router.hasRoute(item.name)) {
-          router.removeRoute(item.name);
-        }
-      });
+      // 在菜单展示全部路由
+      this.routers = [...homepageRouterList, ...accessedRouters, ...fixedRouterList];
+      // 在菜单只展示动态路由和首页
+      // this.routers = [...homepageRouterList, ...accessedRouters];
+      // 在菜单只展示动态路由
+      // this.routers = [...accessedRouters];
     },
-    async restore() {
+    async buildAsyncRoutes() {
+      try {
+        // 发起菜单权限请求 获取菜单列表
+        const asyncRoutes: Array<RouteItem> = (await getMenuList()).list;
+        this.asyncRoutes = transformObjectToRoute(asyncRoutes);
+        await this.initRoutes();
+        return this.asyncRoutes;
+      } catch (error) {
+        throw new Error("Can't build routes");
+      }
+    },
+    async restoreRoutes() {
       this.removeRoutes.forEach((item: RouteRecordRaw) => {
         router.addRoute(item);
       });
