9b9b82e8f0a7a4407ce73499566d3c2cee8438f1
Wen1kang <manhongchung555@gmail.com>
2023-02-02 19:40:52 +0800

    fix: detail style bug

--- src/pages/detail/advanced/constants.ts
+++ src/pages/detail/advanced/constants.ts
@@ -79,7 +79,7 @@ export const BASE_INFO_DATA = [
 
 export const TABLE_COLUMNS_DATA = [
   {
-    width: 300,
+    width: 280,
     ellipsis: true,
     colKey: 'index',
     title: '申请号',
@@ -99,14 +99,14 @@ export const TABLE_COLUMNS_DATA = [
     title: '产品编号',
   },
   {
-    width: 200,
+    width: 160,
     ellipsis: true,
     colKey: 'purchaseNum',
     title: '采购数量',
     sorter: (a, b) => a.purchaseNum - b.purchaseNum,
   },
   {
-    width: 200,
+    width: 160,
     ellipsis: true,
     colKey: 'adminName',
     title: '申请部门',
@@ -131,7 +131,7 @@ export const TABLE_COLUMNS_DATA = [
 export const PRODUCT_LIST = [
   {
     name: 'MacBook Pro 2021',
-    subTitle: 'MacBook Pro 2021',
+    subtitle: '苹果公司（Apple Inc. ）',
     size: '13.3 英寸',
     cpu: 'Apple M1',
     memory: 'RAM 16GB',
@@ -141,7 +141,7 @@ export const PRODUCT_LIST = [
   },
   {
     name: 'Surface Laptop Go',
-    subTitle: '微软（Microsoft Corporation）',
+    subtitle: '微软（Microsoft Corporation）',
     size: '12.4 英寸',
     cpu: 'Core i7',
     memory: 'RAM 16GB',
