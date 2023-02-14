2edd32dcdccf2d8e2551e38a459051dec94cd717
yuyang <uyarnchen@gmail.com>
2023-02-14 10:54:34 +0800

    chore: fix typo

--- src/layouts/components/Footer.vue
+++ src/layouts/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="prefix + '-footer'">Copyright @ 2021-{{ new Date().getFullYear() }} Tencent. All Rights Reserved</div>
+  <div :class="prefix + '-footer'">Copyright © 2021-{{ new Date().getFullYear() }} Tencent. All Rights Reserved</div>
 </template>
 
 <script setup lang="ts">
