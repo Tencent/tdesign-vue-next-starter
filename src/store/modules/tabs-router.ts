cfddfa03bcc235758ae47f917f0d5f5fee2f67ef
liweijie0812 <674416404@qq.com>
2023-02-21 11:36:05 +0800

    feat: 多标签Tab页支持拖拽排序 (#426)
    * feat: 多标签页Tab页支持拖拽排序
    
    * fix: 和HomePage换序 不做处理
    
    * fix: 右键关闭逻辑调整
    
    * chore: 补充类型声明

--- src/store/modules/tabs-router.ts
+++ src/store/modules/tabs-router.ts
@@ -49,17 +49,28 @@ export const useTabsRouterStore = defineStore('tabsRouter', {
     // 处理关闭右侧
     subtractTabRouterBehind(newRoute: TRouterInfo) {
       const { routeIdx } = newRoute;
-      this.tabRouterList = this.tabRouterList.slice(0, routeIdx + 1);
+      const homeIdx: number = this.tabRouters.findIndex((route) => route.isHome);
+      let tabRouterList: Array<TRouterInfo> = this.tabRouterList.slice(0, routeIdx + 1);
+      if (routeIdx < homeIdx) {
+        tabRouterList = tabRouterList.concat(homeRoute);
+      }
+      this.tabRouterList = tabRouterList;
     },
     // 处理关闭左侧
     subtractTabRouterAhead(newRoute: TRouterInfo) {
       const { routeIdx } = newRoute;
-      this.tabRouterList = homeRoute.concat(this.tabRouterList.slice(routeIdx));
+      const homeIdx: number = this.tabRouters.findIndex((route) => route.isHome);
+      let tabRouterList: Array<TRouterInfo> = this.tabRouterList.slice(routeIdx);
+      if (routeIdx > homeIdx) {
+        tabRouterList = homeRoute.concat(tabRouterList);
+      }
+      this.tabRouterList = tabRouterList;
     },
     // 处理关闭其他
     subtractTabRouterOther(newRoute: TRouterInfo) {
       const { routeIdx } = newRoute;
-      this.tabRouterList = routeIdx === 0 ? homeRoute : homeRoute.concat([this.tabRouterList?.[routeIdx]]);
+      const homeIdx: number = this.tabRouters.findIndex((route) => route.isHome);
+      this.tabRouterList = routeIdx === homeIdx ? homeRoute : homeRoute.concat([this.tabRouterList?.[routeIdx]]);
     },
     removeTabRouterList() {
       this.tabRouterList = [];
