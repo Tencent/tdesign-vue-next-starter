d2c87306735705051cf64817b3c785e8ea78b6cb
黎伟杰 <674416404@qq.com>
2023-03-14 10:19:12 +0800

    perf: 接口配置硬编码调整到环境变量

--- src/utils/request/index.ts
+++ src/utils/request/index.ts
@@ -11,7 +11,7 @@ import { ContentTypeEnum } from '@/constants';
 const env = import.meta.env.MODE || 'development';
 
 // 如果是mock模式 或 没启用直连代理 就不配置host 会走本地Mock拦截 或 Vite 代理
-const host = env === 'mock' || !import.meta.env.VITE_IS_REQUEST_PROXY ? '' : import.meta.env.VITE_API_URL;
+const host = env === 'mock' || import.meta.env.VITE_IS_REQUEST_PROXY !== 'true' ? '' : import.meta.env.VITE_API_URL;
 
 // 数据处理，方便区分多种处理方式
 const transform: AxiosTransform = {
