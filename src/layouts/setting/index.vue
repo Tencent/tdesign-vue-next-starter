d91476a6b4588fb03a2eb09f441b98710ddf0695
Uyarn <uyarnchen@gmail.com>
2023-05-10 18:51:35 +0800

    feat: upgrade setting config

--- src/layouts/setting/index.vue
+++ src/layouts/setting/index.vue
@@ -3,10 +3,25 @@
     <!-- 页面配置 -->
     <page-config />
     <!-- 主题皮肤配置 -->
-    <theme-config />
+    <td-theme-generator :props-top="0" :show-setting="true" />
   </div>
 </template>
 <script setup lang="ts">
+import { onMounted } from 'vue';
+
+import { useSettingStore } from '@/store';
+
 import PageConfig from './page-config.vue';
-import ThemeConfig from './theme-config.vue';
+
+const settingStore = useSettingStore();
+
+onMounted(() => {
+  const themeGenerator = document.querySelector('td-theme-generator');
+  themeGenerator.addEventListener('click-setting', () => {
+    settingStore.showSettingPanel = true;
+  });
+  themeGenerator.addEventListener('panel-drawer-visible', (v: CustomEvent) => {
+    if (v?.detail?.[0]) settingStore.showSettingPanel = false;
+  });
+});
 </script>
