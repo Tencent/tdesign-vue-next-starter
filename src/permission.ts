ab6a7164ebf260c57386e5517dc4d91cb2f1aea9
黎伟杰 <674416404@qq.com>
2023-05-11 21:25:16 +0800

    perf: 路由守卫获取菜单异常，跳转登录页并弹窗提示

--- src/permission.ts
+++ src/permission.ts
@@ -25,37 +25,36 @@ router.beforeEach(async (to, from, next) => {
     }
     try {
       await userStore.getUserInfo();
+
+      const { asyncRoutes } = permissionStore;
+
+      if (asyncRoutes && asyncRoutes.length === 0) {
+        const routeList = await permissionStore.buildAsyncRoutes();
+        routeList.forEach((item: RouteRecordRaw) => {
+          router.addRoute(item);
+        });
+
+        if (to.name === PAGE_NOT_FOUND_ROUTE.name) {
+          // 动态添加路由后，此处应当重定向到fullPath，否则会加载404页面内容
+          next({ path: to.fullPath, replace: true, query: to.query });
+        } else {
+          const redirect = decodeURIComponent((from.query.redirect || to.path) as string);
+          next(to.path === redirect ? { ...to, replace: true } : { path: redirect });
+          return;
+        }
+      }
+      if (router.hasRoute(to.name)) {
+        next();
+      } else {
+        next(`/`);
+      }
     } catch (error) {
-      MessagePlugin.error(error);
+      MessagePlugin.error(error.message);
       next({
         path: '/login',
         query: { redirect: encodeURIComponent(to.fullPath) },
       });
       NProgress.done();
-    }
-
-    const { asyncRoutes } = permissionStore;
-
-    if (asyncRoutes && asyncRoutes.length === 0) {
-      const routeList = await permissionStore.buildAsyncRoutes();
-      routeList.forEach((item: RouteRecordRaw) => {
-        router.addRoute(item);
-      });
-
-      if (to.name === PAGE_NOT_FOUND_ROUTE.name) {
-        // 动态添加路由后，此处应当重定向到fullPath，否则会加载404页面内容
-        next({ path: to.fullPath, replace: true, query: to.query });
-      } else {
-        const redirect = decodeURIComponent((from.query.redirect || to.path) as string);
-        next(to.path === redirect ? { ...to, replace: true } : { path: redirect });
-        return;
-      }
-    }
-
-    if (router.hasRoute(to.name)) {
-      next();
-    } else {
-      next(`/`);
     }
   } else {
     /* white list router */
