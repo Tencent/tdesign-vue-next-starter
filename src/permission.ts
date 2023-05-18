b0148c563fb24f9bdb60785cb6d502faf0ee7b51
yuyang <uyarnchen@gmail.com>
2023-05-18 18:11:27 +0800

    chore: sync develop branch to site branch (#513)
    * perf: 路由守卫获取菜单异常，跳转登录页并弹窗提示 (#502)
    
    * perf: 规范不同系统中的结束符 (#505)
    
    * fix: eol
    
    * fix: 测试效果
    
    * fix: 默认全部文件eol=lf
    
    * fix: 防止误判，仅对部分明确的文件设置
    
    * fix: 新建文件eol格式错误进行互补
    
    * Update settings.json
    
    * fix: 修复无法将通知设为未读 (#511)
    
    * fix: 修复 store 中并未对 localStorage 的 TOKEN_NAME 键进行赋值的缺陷 (#504)
    
    * fix: 接口请求不携带token
    
    * fix: 优化token持久化策略
    
    * fix: 删除多余的log
    
    * chore: release 0.7.5 (#512)
    
    ---------
    
    Co-authored-by: liweijie0812 <674416404@qq.com>
    Co-authored-by: Gui.H <springhgui@outlook.com>
    Co-authored-by: You <i@izoyo.cn>

--- src/permission.ts
+++ src/permission.ts
@@ -4,6 +4,7 @@ import NProgress from 'nprogress'; // progress bar
 import { MessagePlugin } from 'tdesign-vue-next';
 import { RouteRecordRaw } from 'vue-router';
 
+import { TOKEN_NAME } from '@/config/global';
 import router from '@/router';
 import { getPermissionStore, getUserStore } from '@/store';
 import { PAGE_NOT_FOUND_ROUTE } from '@/utils/route/constant';
@@ -17,45 +18,44 @@ router.beforeEach(async (to, from, next) => {
   const { whiteListRouters } = permissionStore;
 
   const userStore = getUserStore();
-  const { token } = userStore;
-  if (token) {
+
+  if (userStore[TOKEN_NAME]) {
     if (to.path === '/login') {
       next();
       return;
     }
     try {
       await userStore.getUserInfo();
+
+      const { asyncRoutes } = permissionStore;
+
+      if (asyncRoutes && asyncRoutes.length === 0) {
+        const routeList = await permissionStore.buildAsyncRoutes();
+        routeList.forEach((item: RouteRecordRaw) => {
+          router.addRoute(item);
+        });
+
+        if (to.name === PAGE_NOT_FOUND_ROUTE.name) {
+          // 动态添加路由后，此处应当重定向到fullPath，否则会加载404页面内容
+          next({ path: to.fullPath, replace: true, query: to.query });
+        } else {
+          const redirect = decodeURIComponent((from.query.redirect || to.path) as string);
+          next(to.path === redirect ? { ...to, replace: true } : { path: redirect });
+          return;
+        }
+      }
+      if (router.hasRoute(to.name)) {
+        next();
+      } else {
+        next(`/`);
+      }
     } catch (error) {
-      MessagePlugin.error(error);
+      MessagePlugin.error(error.message);
       next({
         path: '/login',
         query: { redirect: encodeURIComponent(to.fullPath) },
       });
       NProgress.done();
-    }
-
-    const { asyncRoutes } = permissionStore;
-
-    if (asyncRoutes && asyncRoutes.length === 0) {
-      const routeList = await permissionStore.buildAsyncRoutes();
-      routeList.forEach((item: RouteRecordRaw) => {
-        router.addRoute(item);
-      });
-
-      if (to.name === PAGE_NOT_FOUND_ROUTE.name) {
-        // 动态添加路由后，此处应当重定向到fullPath，否则会加载404页面内容
-        next({ path: to.fullPath, replace: true, query: to.query });
-      } else {
-        const redirect = decodeURIComponent((from.query.redirect || to.path) as string);
-        next(to.path === redirect ? { ...to, replace: true } : { path: redirect });
-        return;
-      }
-    }
-
-    if (router.hasRoute(to.name)) {
-      next();
-    } else {
-      next(`/`);
     }
   } else {
     /* white list router */
