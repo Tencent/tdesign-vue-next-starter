12909e5d226c0bf5861ce8fadf7607c8ebdb3c4a
悠静萝莉 <i@mikuhl.cn>
2023-07-08 09:19:42 +0800

    refactor: 重构Axios工具，新增接口级防抖节流 (#556)
    * refactor: 重构Axios工具，新增接口级防抖节流
    
    * fix: 修复编译类型问题
    
    * perf: 优化注释

--- src/utils/request/index.ts
+++ src/utils/request/index.ts
@@ -1,5 +1,5 @@
 // axios配置  可自行根据项目进行更改，只需更改该文件即可，其他文件可以不动
-import type { AxiosInstance, InternalAxiosRequestConfig } from 'axios';
+import type { AxiosInstance } from 'axios';
 import isString from 'lodash/isString';
 import merge from 'lodash/merge';
 
@@ -122,7 +122,7 @@ const transform: AxiosTransform = {
         ? `${options.authenticationScheme} ${token}`
         : token;
     }
-    return config as InternalAxiosRequestConfig;
+    return config;
   },
 
   // 响应拦截器处理
@@ -186,8 +186,10 @@ function createAxios(opt?: Partial<CreateAxiosOptions>) {
           formatDate: true,
           // 是否加入时间戳
           joinTime: true,
-          // 忽略重复请求
-          ignoreRepeatRequest: true,
+          // 是否忽略请求取消令牌
+          // 如果启用，则重复请求时不进行处理
+          // 如果禁用，则重复请求时会取消当前请求
+          ignoreCancelToken: true,
           // 是否携带token
           withToken: true,
           // 重试
