13b4dc4f5c685781f608a72a351afcb204876b27
lcosmos <cosmosliu@foxmail.com>
2023-10-25 15:44:13 +0800

    fix: echarts resize error

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,16 +1,14 @@
 import * as echarts from 'echarts/core';
-import { onMounted, onUnmounted, Ref, ref } from 'vue';
+import { onMounted, onUnmounted, Ref, ref, shallowRef, ShallowRef } from 'vue';
 
 /**
  * eChart hook
  * @param domId
- * @param chart
  */
-export const useChart = (domId: string): Ref<echarts.ECharts> => {
+export const useChart = (domId: string): ShallowRef<echarts.ECharts> => {
   let chartContainer: HTMLCanvasElement;
-  const selfChart = ref<echarts.ECharts | any>();
+  const selfChart = shallowRef<echarts.ECharts | any>();
   const updateContainer = () => {
-    // TODO resize 报错，响应式的问题，待处理
     selfChart.value.resize({
       width: chartContainer.clientWidth,
       height: chartContainer.clientHeight,
