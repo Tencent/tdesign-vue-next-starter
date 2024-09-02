85d5860c5ea069275ec214a23564dad9790ff7ec
悠静萝莉 <i@mikuhl.cn>
2024-09-03 01:37:47 +0800

    refactor(login): 移除i18n相关

--- src/pages/login/components/Login.vue
+++ src/pages/login/components/Login.vue
@@ -9,7 +9,7 @@
   >
     <template v-if="type == 'password'">
       <t-form-item name="account">
-        <t-input v-model="formData.account" size="large" :placeholder="`${$t('pages.login.input.account')}：admin`">
+        <t-input v-model="formData.account" size="large" placeholder="请输入账号：admin">
           <template #prefix-icon>
             <t-icon name="user" />
           </template>
@@ -22,7 +22,7 @@
           size="large"
           :type="showPsw ? 'text' : 'password'"
           clearable
-          :placeholder="`${$t('pages.login.input.password')}：admin`"
+          placeholder="请输入登录密码：admin"
         >
           <template #prefix-icon>
             <t-icon name="lock-on" />
@@ -34,16 +34,16 @@
       </t-form-item>
 
       <div class="check-container remember-pwd">
-        <t-checkbox>{{ $t('pages.login.remember') }}</t-checkbox>
-        <span class="tip">{{ $t('pages.login.forget') }}</span>
+        <t-checkbox>记住账号</t-checkbox>
+        <span class="tip">忘记账号？</span>
       </div>
     </template>
 
     <!-- 扫码登录 -->
     <template v-else-if="type == 'qrcode'">
       <div class="tip-container">
-        <span class="tip">{{ $t('pages.login.wechatLogin') }}</span>
-        <span class="refresh">{{ $t('pages.login.refresh') }} <t-icon name="refresh" /> </span>
+        <span class="tip">请使用微信扫一扫登录</span>
+        <span class="refresh">刷新 <t-icon name="refresh" /> </span>
       </div>
       <qrcode-vue value="" :size="160" level="H" />
     </template>
@@ -51,7 +51,7 @@
     <!-- 手机号登录 -->
     <template v-else>
       <t-form-item name="phone">
-        <t-input v-model="formData.phone" size="large" :placeholder="$t('pages.login.input.phone')">
+        <t-input v-model="formData.phone" size="large" placeholder="请输入手机号码">
           <template #prefix-icon>
             <t-icon name="mobile" />
           </template>
@@ -59,25 +59,21 @@
       </t-form-item>
 
       <t-form-item class="verification-code" name="verifyCode">
-        <t-input v-model="formData.verifyCode" size="large" :placeholder="$t('pages.login.input.verification')" />
+        <t-input v-model="formData.verifyCode" size="large" placeholder="请输入验证码" />
         <t-button size="large" variant="outline" :disabled="countDown > 0" @click="sendCode">
-          {{ countDown == 0 ? $t('pages.login.sendVerification') : `${countDown}秒后可重发` }}
+          {{ countDown == 0 ? '发送验证码' : `${countDown}秒后可重发` }}
         </t-button>
       </t-form-item>
     </template>
 
     <t-form-item v-if="type !== 'qrcode'" class="btn-container">
-      <t-button block size="large" type="submit"> {{ $t('pages.login.signIn') }} </t-button>
+      <t-button block size="large" type="submit"> 登录 </t-button>
     </t-form-item>
 
     <div class="switch-container">
-      <span v-if="type !== 'password'" class="tip" @click="switchType('password')">{{
-        $t('pages.login.accountLogin')
-      }}</span>
-      <span v-if="type !== 'qrcode'" class="tip" @click="switchType('qrcode')">{{
-        $t('pages.login.wechatLogin')
-      }}</span>
-      <span v-if="type !== 'phone'" class="tip" @click="switchType('phone')">{{ $t('pages.login.phoneLogin') }}</span>
+      <span v-if="type !== 'password'" class="tip" @click="switchType('password')">使用账号密码登录</span>
+      <span v-if="type !== 'qrcode'" class="tip" @click="switchType('qrcode')">使用微信扫码登录</span>
+      <span v-if="type !== 'phone'" class="tip" @click="switchType('phone')">使用手机号登录</span>
     </div>
   </t-form>
 </template>
@@ -90,7 +86,6 @@ import { ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 
 import { useCounter } from '@/hooks';
-import { t } from '@/locales';
 import { useUserStore } from '@/store';
 
 const userStore = useUserStore();
@@ -104,10 +99,10 @@ const INITIAL_DATA = {
 };
 
 const FORM_RULES: Record<string, FormRule[]> = {
-  phone: [{ required: true, message: t('pages.login.required.phone'), type: 'error' }],
-  account: [{ required: true, message: t('pages.login.required.account'), type: 'error' }],
-  password: [{ required: true, message: t('pages.login.required.password'), type: 'error' }],
-  verifyCode: [{ required: true, message: t('pages.login.required.verification'), type: 'error' }],
+  phone: [{ required: true, message: '手机号必填', type: 'error' }],
+  account: [{ required: true, message: '账号必填', type: 'error' }],
+  password: [{ required: true, message: '密码必填', type: 'error' }],
+  verifyCode: [{ required: true, message: '验证码必填', type: 'error' }],
 };
 
 const type = ref('password');
--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -4,12 +4,12 @@
 
     <div class="login-container">
       <div class="title-container">
-        <h1 class="title margin-no">{{ $t('pages.login.loginTitle') }}</h1>
+        <h1 class="title margin-no">登录到</h1>
         <h1 class="title">TDesign Starter</h1>
         <div class="sub-title">
-          <p class="tip">{{ type == 'register' ? $t('pages.login.existAccount') : $t('pages.login.noAccount') }}</p>
+          <p class="tip">{{ type == 'register' ? '已有账号?' : '没有账号吗?' }}</p>
           <p class="tip" @click="switchType(type == 'register' ? 'login' : 'register')">
-            {{ type == 'register' ? $t('pages.login.signIn') : $t('pages.login.createAccount') }}
+            {{ type == 'register' ? '登录' : '注册新账号' }}
           </p>
         </div>
       </div>
