257d8f90b8834777d8e5af582e8a288fdddbbc95
悠静萝莉 <i@mikuhl.cn>
2024-09-04 16:54:58 +0800

    fix(breadcrumb): 修复多层级路由指向错误(#749)

--- src/layouts/components/Breadcrumb.vue
+++ src/layouts/components/Breadcrumb.vue
@@ -27,7 +27,7 @@ const crumbs = computed(() => {
 
     breadcrumbArray.push({
       path,
-      to: breadcrumbArray[idx - 1] ? `/${breadcrumbArray[idx - 1].path}/${path}` : `/${path}`,
+      to: breadcrumbArray[idx - 1] ? `${breadcrumbArray[idx - 1].to}/${path}` : `/${path}`,
       title: route.matched[idx]?.meta?.title ?? path,
     });
     return breadcrumbArray;
