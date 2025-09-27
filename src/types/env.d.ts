252d491a417ab3575004ef44f3d6942955b3a82c
SharerMax <mdcw1103@gmail.com>
2025-09-27 16:07:33 +0800

    fix: type of `ImportMetaEnv`  not effective

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,5 +1,9 @@
-export interface ImportMetaEnv {
+interface ImportMetaEnv {
   readonly VITE_IS_REQUEST_PROXY: string;
   readonly VITE_API_URL: string;
   readonly VITE_API_URL_PREFIX: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
 }
